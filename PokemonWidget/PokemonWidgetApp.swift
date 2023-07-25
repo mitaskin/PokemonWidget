4e10dc85a3b42f7dfefdee03a1b17e3d8b6d7773
mitaskin <mi.taskin027@gmail.com>
2023-07-25 14:36:25 +0300

    Initial Commit

---
+++ PokemonWidget/PokemonWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonWidgetApp.swift
+//  PokemonWidget
+//
+//  Created by Muhammed ikbal TAŞKIN on 25.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct PokemonWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
